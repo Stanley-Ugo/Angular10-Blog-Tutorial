d27dd95be691c7e0ad47bc793a37ed887809fa54
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-03-16 00:35:57 +0100

    Creating a custom directive with the CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomeStyleDirective } from './custome-style.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomeStyleDirective
   ],
   imports: [
     BrowserModule,
